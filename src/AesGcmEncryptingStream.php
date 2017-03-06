66eaf7e95a593d96f3892bf191e1275cdd57f1e6
Jonathan Eskew <jonathan@jeskew.net>
2017-03-05 16:23:09 -0800

    Add 'streaming' AES-GCM implementation
    The API exposed by PHP's OpenSSL bindings does not appear to allow
    incremental encryption or decryption of AEAD ciphers. The implementation
    included loads the entire source stream into memory and writes the
    plaintext (in the case of decryption) or ciphertext (in the case of
    encryption) into a php://temp stream

---
+++ src/AesGcmEncryptingStream.php
@@ -0,0 +1,65 @@
+<?php
+namespace Jsq\EncryptionStreams;
+
+use GuzzleHttp\Psr7;
+use GuzzleHttp\Psr7\StreamDecoratorTrait;
+use Psr\Http\Message\StreamInterface;
+
+class AesGcmEncryptingStream implements StreamInterface
+{
+    use StreamDecoratorTrait;
+
+    private $aad;
+
+    private $initializationVector;
+
+    private $key;
+
+    private $keySize;
+
+    private $plaintext;
+
+    private $tag = '';
+
+    private $tagLength;
+
+    public function __construct(
+        StreamInterface $plaintext,
+        string $key,
+        string $initializationVector,
+        string $aad = '',
+        int $tagLength = 16,
+        int $keySize = 256
+    ) {
+        $this->plaintext = $plaintext;
+        $this->key = $key;
+        $this->initializationVector = $initializationVector;
+        $this->aad = $aad;
+        $this->tagLength = $tagLength;
+        $this->keySize = $keySize;
+    }
+
+    public function createStream(): StreamInterface
+    {
+        return Psr7\stream_for(openssl_encrypt(
+            (string) $this->plaintext,
+            "aes-{$this->keySize}-gcm",
+            $this->key,
+            OPENSSL_RAW_DATA,
+            $this->initializationVector,
+            $this->tag,
+            $this->aad,
+            $this->tagLength
+        ));
+    }
+
+    public function getTag(): string
+    {
+        return $this->tag;
+    }
+
+    public function isWritable(): bool
+    {
+        return false;
+    }
+}
